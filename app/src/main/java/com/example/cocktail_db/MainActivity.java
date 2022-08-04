57aadd6f60ced519374fe7289b83446526efe6cb
BuildTools <unconfigured@null.spigotmc.org>
2022-08-04 19:32:36 -0400

    Created the main page (Still WIP)

--- app/src/main/java/com/example/cocktail_db/MainActivity.java
+++ app/src/main/java/com/example/cocktail_db/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.cocktail_db;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    searchFragment fragment = new searchFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_button:
+                Toast.makeText(getApplicationContext(), "You Clicked on Item 1", Toast.LENGTH_SHORT).show();
+                FragmentManager fm =  getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.main_fragment,searchFragment.class,null).commit();
+
+            default:
+                return false;
+        }
+    }
+
 }
